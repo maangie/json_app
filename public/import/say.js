3bde99978854f4ab4ec0ca07b8237b61445a0a6e
maangie <maangie906@gmail.com>
2019-09-02 14:20:25 +0900

    /import/ 追加

---
+++ public/import/say.js
@@ -0,0 +1,11 @@
+export function hi() {
+  console.log('hello');
+}
+
+export function bye() {
+  console.log('bye');
+}
+
+export default function() {
+  console.log('Module loaded (export default)');
+}
